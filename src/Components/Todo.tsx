69b13ff49ef48fe221270927cddf2e40b0a19322
Shivansh2203 <168472217+Shivansh2203@users.noreply.github.com>
2025-05-13 14:31:58 +0530

    Your commit message here

--- src/Components/Todo.tsx
+++ src/Components/Todo.tsx
@@ -12,7 +12,7 @@ type TodoProp = {
 
 const Todo = ({ todos,completeTodo,deleteTodo}:TodoProp) => {
   return (
-    <div className="bg-purple-800 p-2 rounded-md flex justify-between items-center text-white">
+    <div className="bg-purple-800 p-2 m-2 rounded-md flex justify-between items-center text-white">
       <p className={`${todos.completed ===true ?"line-through" : ""}`}>{todos.text}</p>
       <div className="flex items-center gap-2 cursor-pointer">
         <FaCheckCircle className="hover:text-gray-400" onClick={() => 
